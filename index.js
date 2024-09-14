a8ca4f7833f632f1cc134df71b643e250e12519f
BKharel <101487496+buddha2042@users.noreply.github.com>
2024-09-14 17:57:14 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+function fetchDataAndUpdate() {
+        const pageCode = './store/pageContent.json'
+        fetch(pageCode)
+            .then(response => response.json())
+            .then(data => {
+                const bodyContent = data.page;
+                const bodyElement = document.getElementById('body');
+                bodyElement.innerHTML = bodyContent;
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    }
+    
+    fetchDataAndUpdate();
+    
+    function showAlert(){
+        alert('This is a fully automated app, no programmer is working behind this application.');
+    }
